da24c2c808545092cd07c13ad8aea6a32473172f
MattssonHubGit <erik.mattsson@live.se>
2018-11-28 18:44:31 +0100

    Folders added

---
+++ SpellCraftingV2/Assets/Scripts/Abstract/Item.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Item : MonoBehaviour
+{
+    [Header("Item")]
+    [SerializeField] protected string itemName = "";
+    [SerializeField] protected int itemID;
+    [SerializeField] protected bool bagable = true;
+    [Space]
+    [SerializeField] protected Sprite inventoryIcon;
+    [SerializeField] protected Mesh worldMesh;
+    [SerializeField] protected Material worldMaterial;
+
+    protected void InWorldAnimation(){}
+    protected void PickUp(){}
+    protected abstract void UseItem();
+
+}
