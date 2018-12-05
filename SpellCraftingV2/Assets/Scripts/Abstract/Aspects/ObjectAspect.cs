cacccbd4cd846294eb432d6832cf79be8871aeb1
MattssonHubGit <erik.mattsson@live.se>
2018-12-05 22:25:49 +0100

    Continued adding aspects for a test spell, this time a fist with knockback on touch was added

--- SpellCraftingV2/Assets/Scripts/Abstract/Aspects/ObjectAspect.cs
+++ SpellCraftingV2/Assets/Scripts/Abstract/Aspects/ObjectAspect.cs
@@ -4,14 +4,24 @@ using UnityEngine;
 
 public abstract class ObjectAspect : Aspect
 {
+    //TODO: Make the aspect enter/exit the crafting/inventory
+
     [Header("Object Aspect")]
     [SerializeField] protected GameObject objectPrefab;
 
 
     [HideInInspector] public CoreRune Rune;
 
-    protected abstract void InnateEffect();
+    public virtual void SetInnateEffect(GameObject applyTo)
+    {
+        applyTo.GetComponent<InnateEffect>().data = this;
+    }
     protected abstract void OnDestruction();
     protected abstract void OnCreate();
 
+
+    public override void UseItem()
+    {
+        Debug.Log("UseItem() not implemented in ObjectAspect");
+    }
 }
---
+++ SpellCraftingV2/Assets/Scripts/Abstract/InnateEffect.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class should contain a effect/innate behaivour that applies to a gameobject it's script is applied to. Should be used on core objects
+/// </summary>
+[System.Serializable]
+public abstract class InnateEffect : MonoBehaviour
+{
+    [SerializeField] public ScriptableObject data;
+}
